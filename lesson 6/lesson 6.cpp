2465101a8e75247e8b7b1ccca47640187cc41a87
Nazar Shpak <nazar.shpak2004@gmail.com>
2022-04-09 22:43:21 +0300

    Add project files.

---
+++ lesson 6/lesson 6.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <cstdlib>
+
+int main()
+{
+    std::cout << "Enter lenth: ";
+    int a;
+    std::cin >> a;
+   
+    int *arr = new int[a];
+    std::cout << "Enter num: ";
+    int num;
+    std::cin >> num;
+    int b=0;
+    for (int i = 0; i < a; i++)
+    {
+        arr[i] = rand()%100; 
+        std::cout << arr[i] << std::endl;
+    }
+    for (int i = 0; i < a; i++)
+    {
+        if (arr[i] == num)
+        {
+            std::cout << " this number is in the array";
+            b = 1;
+        }
+    }
+       if(b!=1)
+        std::cout <<"this number is not in the array" << std::endl;
+    
+    return 0;
+}
